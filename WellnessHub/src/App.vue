6da7bda5cb10284d4dee4f3c99c5173cf9df02b8
userr1010 <netharaathukorala10@gmail.com>
2024-08-31 19:48:45 +1000

    feat: SignUp added with validation

--- WellnessHub/src/App.vue
+++ WellnessHub/src/App.vue
@@ -2,11 +2,13 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import HomeView from './views/HomeView.vue'
+import SignUpView from './views/SignUpView.vue'
 
 </script>
 
 <template>
-    <HomeView/>
+    <!-- <HomeView/> -->
+    <SignUpView/>
 </template>
 
 <style scoped>
